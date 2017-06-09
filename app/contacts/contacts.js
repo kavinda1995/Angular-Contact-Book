609c35206fb927b630db94592f7235ef981565d2
Kavinda Jayakody <kavindajayakody@gmail.com>
2017-06-09 06:40:08 +0530

    init

---
+++ app/contacts/contacts.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('myApp.contacts', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'ContactsCtrl'
+  });
+}])
+
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+	//var ref = new Firebase('https://angular-contactlist.firebaseio.com/');
+
+	console.log($firebaseArray); //<- verified here that the injection works
+	var ref = firebase.database().ref();
+
+	$scope.contacts = $firebaseArray(ref);
+
+	$scope.addFormShow = true;
+	$scope.editFormShow = false;
+
+	$scope.showEditForm = function(contact){
+		$scope.addFormShow = false;
+		$scope.editFormShow = true;
+
+		$scope.id = contact.$id;
+		$scope.name = contact.name;
+		$scope.email = contact.email;
+		$scope.phone = contact.phone;
+	}
+
+	$scope.addContact = function(){
+		console.log('Adding Contacts....')
+
+		$scope.contacts.$add({
+			name: $scope.name,
+			email: $scope.email,
+			phone: $scope.phone
+		}).then(function(ref){
+			var id = ref.key;
+			console.log('Added Contact'+id);
+
+			$scope.name = '';
+			$scope.email = '';
+			$scope.phone = '';
+		});
+	}
+
+	$scope.editContact =  function(){
+		var id = $scope.id;
+
+		var record = $scope.contacts.$getRecord(id);
+
+		record.name = $scope.name;
+		record.email = $scope.email;
+		record.phone = $scope.phone;
+
+		//Save
+		$scope.contacts.$save(record).then(function(ref){
+			console.log(ref.key);
+		});
+
+		$scope.name = '';
+		$scope.email = '';
+		$scope.phone = '';
+	}
+
+	$scope.removeContact = function(contact){
+		$scope.contacts.$remove(contact);
+	}
+
+}]);
